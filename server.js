43971d890ac4ffe8a7044ea56962cf0205d8fc83
camillebagnani <140850865+camillebagnani@users.noreply.github.com>
2024-01-12 15:16:24 -0700

    worked on express js responses to index and notes

--- server.js
+++ server.js
@@ -10,14 +10,22 @@ const app = express();
 
 const PORT = 3001;
 
+const notesData = require('./db/db.json')
+
 // Middleware points to the public folder where all the static files are
 // Middleware is a starting point to minimize paths -- EDIT THIS NOTE
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => 
-res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+});
 
-app.listen(PORT, () =>
-console.log(`Listening at http://localhost:${PORT}`)
-)
+app.get('api/notes', (req, res) => res.json(notesData));
+
+// app.get('/api/notes', (req, res) => {
+// res.sendFile(path.join(__dirname, 'public/notes.html'))
+// });
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`)
+});
